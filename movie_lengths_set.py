0f047c49b6d14ee0be8c33b449035e280c3a74f2
Casey Gorman <caseyagorman@gmail.com>
2019-03-07 21:26:11 -0800

    add movie lengths set

---
+++ movie_lengths_set.py
@@ -0,0 +1,7 @@
+def exact_length_flight(flight_length, movie_lengths):
+    movie_lengths_seen = set()
+    for movie in movie_lenghts:
+        match = flight_length - movie
+        if match in movie_lengths_seen:
+            return True
+        movie_lengths_seen.add(movie)
